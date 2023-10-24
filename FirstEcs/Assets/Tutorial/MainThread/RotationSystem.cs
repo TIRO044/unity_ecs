ab881eade2d24e6cf9f0375a79a1e3bec0a7ba12
emddldans <wjdqkddl044@naver.com>
2023-10-24 17:58:40 +0900

    1.MainThread - Rotation Cube

--- FirstEcs/Assets/Tutorial/MainThread/RotationSystem.cs
+++ FirstEcs/Assets/Tutorial/MainThread/RotationSystem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class First_Ecs : MonoBehaviour
+public class RotationSystem : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
